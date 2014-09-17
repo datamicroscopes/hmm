715010e40f79b0ef3cb4e0f65a99758042e5b88b
David Pfau <david@qadium.com>
2014-09-17 19:25:44 -0400

    Ditched the direct assignment sampler because mixing would be too slow. Started implementation of beam sampler.

--- include/microscopes/hmm/model.hpp
+++ include/microscopes/hmm/model.hpp
@@ -1,34 +1,84 @@
 #pragma once
 
+#include <distributions/special.hpp>
+
+#include <vector>
+
 namespace microscopes{
 namespace hmm{
 
-typedef std::vector<std::vector<size_t>> data_type; // shorten type
-// To start with, just build an HDP-HMM for discrete distributions with Dirichlet prior on observations
-class hmm {
-public:
-	hdp_hmm(size_t alphalen, data_type data) : 
-		alphalen_(alphalen), 
-		gamma_(),
-		alpha0_(),
-		obs_hp_(alphalen, 1.0 / alphalen)
-		data_(data)
-	{}
-protected:
-	// hyperparameters
-	const size_t alphalen_; // alphabet size
-	float gamma_; // top-level concentration parameter
-	float alpha0_; // lower-level concentration parameter
-	std::vector<double> obs_hp_; // vector of Dirichlet distribution hyperparameters for prior on observation distribution
+	// A class for a vector of vectors, useful for representing pretty much everything we need for the beam sampler.
+	// For instance, time series data can be stored as a vector of vectors, where each vector is one time series.
+	// The transition matrix can also be stored as a vector of vectors, where each transition probability is one vector.
+	template <typename T>
+	class meta_vector {
+	public:
 
-	// data
-	const data_type data_;
+		meta_vector(): data_() {}
 
-	// parameters updated by Gibbs sampling
-	data_type assignments_; // assignment of every time step in the data to a latent state
-	std::map<std::pair<size_t, size_t>, size_t> table_counts_; // (dish_id, restaurant_id) maps to number of tables in that restaurant that serve that dish
-	std::vector<double> betas_; // stick lengths for upper level Dirichlet Process sample
-};
+		meta_vector(std::vector<size_t> size) : data_(size.size()) {
+			for (int i = 0; i < data_.size(); i++) {
+				data_[i] = std::vector<T>(size[i]);
+			}
+		}
 
-} // namespace hdp_hmm
+		std::vector<T>& operator[](size_t i) {
+			return data_[i];
+		}
+
+		std::vector<size_t> size() {
+			std::vector<size_t> sizes(data_.size());
+			for (std::vector<T> vec: data_) {
+				sizes.push_back(vec.size());
+			}
+			return sizes;
+		}
+
+		T sum(size_t i) { // useful for resampling m, the number of tables serving a dish
+			T result = (T)0;
+			for(T t: data_[i]) {
+				result += t;
+			}
+			return result;
+		}
+	protected:
+		std::vector<std::vector<T> > data_;
+	};
+
+// Implementation of the beam sampler for the HDP-HMM, following van Gael 2008
+	class hmm {
+	public:
+		hmm(size_t alphalen, float gamma, float alpha0, float *H, meta_vector<size_t> data):
+			gamma_(gamma),
+			alpha0_(alpha0),
+			H_(H),
+			alphalen_(alphalen),
+			data_(data),
+			u_(data.size())
+		{}
+	protected:
+		
+		// parameters
+		const size_t alphalen_;
+		const meta_vector<size_t> data_; // XXX: For now, the observation type is just a vector of vectors of ints. Later we can switch over to using recarrays
+		meta_vector<float> u_; // the slice sampling parameter for each time step in the series
+		meta_vector<float> pi_; // the observed portion of the infinite transition matrix
+		std::vector<float> beta_; // the stick lengths for the top-level DP draw
+
+		// hyperparameters
+		const float gamma_;
+		const float alpha0_;
+		const float *H_; // hyperparameters for a Dirichlet prior over observations. Will generalize this to other observation models later.
+
+		// sampling functions. later we can integrate these into microscopes::kernels where appropriate.
+		void forward_backward() {}
+
+		void sample_u() {}
+
+		void sample_pi() {}
+
+		void sample_beta() {}
+	};
+
+} // namespace hmm
 } // namespace microscopes
--- src/hmm/model.cpp
+++ src/hmm/model.cpp
@@ -1,1 +1,6 @@
 #include <microscopes/hmm/model.hpp>
+
+// kill this as soon as we integrate this into the rest of the build
+int main() {
+	return 0; 
+}
