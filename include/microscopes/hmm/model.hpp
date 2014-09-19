6ce3cc39c4732cafd415cd16ac1b5441da11599d
David Pfau <david@qadium.com>
2014-09-18 21:45:21 -0400

    Implemented update for slice sampling auxilliary variable

--- include/microscopes/hmm/model.hpp
+++ include/microscopes/hmm/model.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <distributions/special.hpp>
+#include <distributions/random.hpp>
+#include <distributions/random_fwd.hpp>
 
 #include <vector>
 
 namespace microscopes{
 namespace hmm{
+
+	distributions::rng_t rng;
 
 	// A class for a vector of vectors, useful for representing pretty much everything we need for the beam sampler.
 	// For instance, time series data can be stored as a vector of vectors, where each vector is one time series.
@@ -46,21 +50,21 @@ namespace hmm{
 	};
 
 // Implementation of the beam sampler for the HDP-HMM, following van Gael 2008
+	template <int N>
 	class hmm {
 	public:
-		hmm(size_t alphalen, float gamma, float alpha0, float *H, meta_vector<size_t> data):
+		hmm(float gamma, float alpha0, float *H, meta_vector<size_t> data):
 			gamma_(gamma),
 			alpha0_(alpha0),
 			H_(H),
-			alphalen_(alphalen),
 			data_(data),
 			u_(data.size())
 		{}
 	protected:
 		
 		// parameters
-		const size_t alphalen_;
 		const meta_vector<size_t> data_; // XXX: For now, the observation type is just a vector of vectors of ints. Later we can switch over to using recarrays
+		meta_vector<size_t> s_; // the state sequence
 		meta_vector<float> u_; // the slice sampling parameter for each time step in the series
 		meta_vector<float> pi_; // the observed portion of the infinite transition matrix
 		std::vector<float> beta_; // the stick lengths for the top-level DP draw
@@ -68,12 +72,26 @@ namespace hmm{
 		// hyperparameters
 		const float gamma_;
 		const float alpha0_;
-		const float *H_; // hyperparameters for a Dirichlet prior over observations. Will generalize this to other observation models later.
+		const float H_[N]; // hyperparameters for a Dirichlet prior over observations. Will generalize this to other observation models later.
 
 		// sampling functions. later we can integrate these into microscopes::kernels where appropriate.
-		void forward_backward() {}
+		void sample_s() {}
 
-		void sample_u() {}
+		void sample_u() {
+			size_t prev_state;
+			std::uniform_real_distribution<float> sampler (0.0, 1.0);
+			std::vector<size_t> sizes = u_.size();
+			for (int i = 0; i < sizes.size(); i++) {
+				for(int j = 0; j < sizes[i]; j++) {
+					if (j == 0) {
+						prev_state = 0;
+					} else {
+						prev_state = s_[i][j-1];
+					}
+					u_[i][j] = sampler(rng) / (pi_[prev_state][s_[i][j]]); // scale the uniform sample to be between 0 and pi_{s_{t-1}s_t}
+				}
+			}
+		}
 
 		void sample_pi() {}
 
