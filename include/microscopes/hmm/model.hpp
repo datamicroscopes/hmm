f3018d3d13c3ddbbc2f6f44e74572c4dd2073ae6
David Pfau <david@qadium.com>
2014-10-03 16:59:54 -0400

    Added stick-breaking after sampling the auxilliary variables, so that every state visited by the beam sampler is instantiated

--- include/microscopes/hmm/model.hpp
+++ include/microscopes/hmm/model.hpp
@@ -17,7 +17,6 @@ namespace hmm{
   // For instance, time series data can be stored as a vector of vectors, where each vector is one time series.
   // The transition matrix can also be stored as a vector of vectors, where each transition probability is one vector.
 
-  // Though I realize now, once I am adding-and-removing states, it might make sense to replace this with group_manager.
   template <typename T>
   class meta_vector {
   public:
@@ -32,6 +31,10 @@ namespace hmm{
 
     std::vector<T>& operator[](size_t i) {
       return data_[i];
+    }
+
+    void push_back(std::vector<T> vec) {
+      data_.push_back(vec);
     }
 
     std::vector<size_t> size() {
@@ -64,9 +67,16 @@ namespace hmm{
       data_(data),
       u_(data.size()),
       beta_(),
-      memoized_log_stirling_(),
-      states_()
+      memoized_log_stirling_()
     {
+    }
+
+    void sample_beam() {
+      sample_u();
+      sample_s();
+      sample_pi();
+      sample_phi();
+      sample_beta();
     }
   protected:
     
@@ -77,13 +87,15 @@ namespace hmm{
     meta_vector<size_t> s_; // the state sequence
     meta_vector<float> u_; // the slice sampling parameter for each time step in the series
 
-    // these three all have the same shape as the transition matrix
-    meta_vector<size_t> m_; // auxilliary variable necessary for sampling beta
-    meta_vector<size_t> counts_; // the count of how many times a transition occurs between states
-    meta_vector<float> pi_; // the observed portion of the infinite transition matrix
+    // these three all have the same shape as the transition matrix, approximately
+    meta_vector<size_t> m_; // auxilliary variable necessary for sampling beta. Size K x K.
+    meta_vector<size_t> counts_; // the count of how many times a transition occurs between states. Size K x K.
+    meta_vector<float> pi_; // the observed portion of the infinite transition matrix. Size K x K+1.
+
+    meta_vector<float> phi_; // the emission matrix. Size K x N.
 
     // shape is the number of states currently instantiated
-    std::vector<float> beta_; // the stick lengths for the top-level DP draw
+    std::vector<float> beta_; // the stick lengths for the top-level DP draw. Size K+1.
 
     // hyperparameters
     const float gamma_;
@@ -92,20 +104,25 @@ namespace hmm{
 
     // helper fields
     std::map<size_t, std::vector<float> > memoized_log_stirling_; // memoize computation of log stirling numbers for speed when sampling m
-    std::set<size_t> states_;
+    // Over all instantiated states, the maximum value of the part of pi_k that belongs to the "unseen" states. 
+    //Should be smaller than the least value of the auxiliary variable, so all possible states visited by the beam sampler are instantiated
+    float max_pi; 
 
     // sampling functions. later we can integrate these into microscopes::kernels where appropriate.
     void sample_s() {
+      counts_ = meta_vector<size_t>(); // clear counts
+
       std::vector<size_t> sizes = data_.size();
-      counts_();
       for (int i = 0; i < sizes.size(); i++) {
         // Forward-filter
-        for (int t = 0; t < size[i]; t++) {
+        size_t last_state = 0; // always initialize at zero state
+        meta_vector<float> forward_probs;
+        for (int t = 0; t < sizes[i]; t++) {
 
         }
 
         // Backwards-sample
-        for (int t = size[i]-1; t >= 0; t--) {
+        for (int t = sizes[i]-1; t >= 0; t--) {
 
         }
       }
@@ -115,6 +132,7 @@ namespace hmm{
       size_t prev_state;
       std::uniform_real_distribution<float> sampler (0.0, 1.0);
       std::vector<size_t> sizes = u_.size();
+      float min_u = 1.0; // used to figure out where to truncate sampling of pi
       for (int i = 0; i < sizes.size(); i++) {
         for(int j = 0; j < sizes[i]; j++) {
           if (j == 0) {
@@ -123,24 +141,59 @@ namespace hmm{
             prev_state = s_[i][j-1];
           }
           u_[i][j] = sampler(rng) / (pi_[prev_state][s_[i][j]]); // scale the uniform sample to be between 0 and pi_{s_{t-1}s_t}
+          min_u = min_u < u_[i][j] ? min_u : u_[i][j];
         }
+      }
+
+      // If necessary, break the pi stick some more
+      while (max_pi > min_u) {
+        // Add new state
+        size_t K = pi_.size().size();
+        pi_.push_back(std::vector<float>(K+1));
+        sample_pi_row(K, K);
+
+        // Break beta stick
+        float bu = beta_[K];
+        float bk = distributions::sample_beta(rng, 1.0, gamma_);
+        beta_[K] = bu * bk;
+        beta_.push_back(bu * (1-bk));
+
+        // Add new transition to each state
+        for (int i = 0; i < K+1; i++) {
+          float pu = pi_[i][K];
+          float pk = distributions::sample_beta(rng, alpha0_ * beta_[K], alpha0_ * beta_[K+1]);
+          pi_[i][K] = pu * pk;
+          pi_[i].push_back(pu * (1-pk));
+        }
+
+        // check that sizes are all consistent
       }
     }
 
     void sample_pi() {
-      std::vector<size_t> sizes = u_.size();
-      for (int i = 0; i < sizes.size(); i++) {
-        float new_pi[sizes[i]+1];
-        float alphas[sizes[i]+1];
-        for (int j = 0; j < sizes[i]; j++) {
+      size_t K = pi_.size().size();
+      max_pi = 0.0;
+      for (int i = 0; i < K; i++) {
+        sample_pi_row(i, K);
+      }
+    }
+
+    void sample_pi_row(size_t i, size_t K) {
+        float new_pi[K+1];
+        float alphas[K+1];
+        for (int j = 0; j < K; j++) {
           alphas[j] = counts_[i][j] + alpha0_ * beta_[j];
         }
-        alphas[sizes[i]] = alpha0_ * beta_[sizes[i]];
-        distributions::sample_dirichlet(rng, sizes[i]+1, alphas, new_pi);
-        for (int j = 0; j < sizes[i]; j++) {
+        alphas[K] = alpha0_ * beta_[K];
+        distributions::sample_dirichlet(rng, K+1, alphas, new_pi);
+        for (int j = 0; j < K+1; j++) {
           pi_[i][j] = new_pi[i];
         }
-      }
+        max_pi = max_pi > new_pi[K] ? max_pi : new_pi[K];
+    }
+
+    void sample_phi() {
+
     }
 
     void sample_m() {
@@ -165,6 +218,7 @@ namespace hmm{
     }
 
     void sample_beta() {
+      sample_m();
       size_t K = m_.size().size();
       float alphas[K+1];
       float new_beta[K+1];
